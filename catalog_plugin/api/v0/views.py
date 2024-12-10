49987c178f2dae2137f255ff0c9fe666f6f6f13a
anfbermudezme <andres.bermudez@edunext.co>
2024-12-10 20:34:35 +0100

    feat: add REST API for FlexibleCatalogModel.

--- catalog_plugin/api/v0/views.py
+++ catalog_plugin/api/v0/views.py
@@ -4,13 +4,20 @@ from edx_rest_framework_extensions.permissions import IsAuthenticated, IsStaff
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
-from catalog_plugin.models import AvailableCourse, FixedCatalog, CatalogCourses
+from catalog_plugin.models import (
+    FlexibleCatalogModel,
+    AvailableCourse,
+    FixedCatalog,
+    CatalogCourses,
+)
 from catalog_plugin.api.v0.serializers import (
+    FlexibleCatalogSerializer,
     AvailableCourseSerializer,
     FixedCatalogSerializer,
     CatalogCoursesSerializer,
 )
 from catalog_plugin.api.v0.filters import (
+    FlexibleCatalogFilter,
     AvailableCourseFilter,
     FixedCatalogFilter,
     CatalogCoursesFilter,
@@ -32,6 +39,23 @@ class AvailableCourseViewSet(viewsets.ModelViewSet):
     search_fields = ['course__name']
     ordering_fields = ['course', 'active', 'id']
     ordering = ['id']
+
+
+class FlexibleCatalogViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing FlexibleCatalogModel instances.
+    """
+
+    authentication_classes = (JwtAuthentication,)
+    permission_classes = (IsAuthenticated, IsStaff)
+    queryset = FlexibleCatalogModel.objects.all()
+    serializer_class = FlexibleCatalogSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = FlexibleCatalogFilter
+    search_fields = ['name', 'slug']
+    ordering_fields = ['name', 'slug', 'id']
+    ordering = ['name']
 
 
 class FixedCatalogViewSet(viewsets.ModelViewSet):
